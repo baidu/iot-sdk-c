e66c408603e9d3cb42f70fb3d451930a82f5c42e
Zhao Bo <zhaobo03@baidu.com>
2017-09-28 15:23:54 +0800

    fix comments in BOS.h.

--- iothub_client/inc/bos.h
+++ iothub_client/inc/bos.h
@@ -46,19 +46,35 @@ DEFINE_ENUM(BOS_RESULT, BOS_RESULT_VALUES)
 #define IS_SUCCESS_STATUS(code) ((code) >= 200 && (code) < 300)
 
 /**
-* @brief	Synchronously uploads a byte array to blob storage
+* @brief	Synchronously uploads a byte array to BOS
 *
-* @param	SASURI	        The URI to use to upload data
-* @param	size		    The size of the data to be uploaded (can be 0)
+* @param	host	        host of BOS server. For example 'bj.bcebos.com' or 'gz.bcebos.com'
+* @param	ak	            Access key ID.
+* @param	sk	            Secret key.
+* @param	bucket		    BOS bucket to upload to. For BOS core concepts please refer to https://cloud.baidu.com/doc/BOS/ProductDescription.html#.E6.A0.B8.E5.BF.83.E6.A6.82.E5.BF.B5
+* @param	objectKey		BOS object's key to upload to. Like 'folder1/folder2/my-file.txt' or 'my-file.txt'.
 * @param	source		    A pointer to the byte array to be uploaded (can be NULL, but then size needs to be zero)
-* @param    httpStatus      A pointer to an out argument receiving the HTTP status (available only when the return value is BLOB_OK)
-* @param    httpResponse    A BUFFER_HANDLE that receives the HTTP response from the server (available only when the return value is BLOB_OK)
-* @param    certificates    A null terminated string containing CA certificates to be used
+* @param	size		    Size of the source
+* @param    httpStatus      A pointer to an out argument receiving the HTTP status (available only when the return value is BOS_OK)
+* @param    httpResponse    A BUFFER_HANDLE that receives the HTTP response from the server (available only when the return value is BOS_OK)
 *
-* @return	A @c BLOB_RESULT. BLOB_OK means the blob has been uploaded successfully. Any other value indicates an error
+* @return	A @c BOS_RESULT.  BOS_OK means the object has been uploaded successfully. Any other value indicates an error
 */
 MOCKABLE_FUNCTION(, BOS_RESULT, BOS_Upload, const char*, host, const char *, ak, const char *, sk, const char *, bucket, const char *, objectKey, const unsigned char*, source, size_t, size, unsigned int*, httpStatus, BUFFER_HANDLE, httpResponse)
 
+/**
+* @brief	Synchronously download an object from BOS
+*
+* @param	host	        host of BOS server. For example 'bj.bcebos.com' or 'gz.bcebos.com'
+* @param	ak	            Access key ID.
+* @param	sk	            Secret key.
+* @param	bucket		    BOS bucket to download from. For BOS core concepts please refer to https://cloud.baidu.com/doc/BOS/ProductDescription.html#.E6.A0.B8.E5.BF.83.E6.A6.82.E5.BF.B5
+* @param	objectKey		BOS object's key to download from. Like 'folder1/folder2/my-file.txt' or 'my-file.txt'.
+* @param    httpStatus      A pointer to an out argument receiving the HTTP status (available only when the return value is BOS_OK)
+* @param    httpResponse    A BUFFER_HANDLE that receives the HTTP response from the server (available only when the return value is BOS_OK). Actual content of the BOS object is in it.
+*
+* @return	A @c BOS_RESULT.  BOS_OK means the object has been uploaded successfully. Any other value indicates an error
+*/
 MOCKABLE_FUNCTION(, BOS_RESULT, BOS_Download, const char*, host, const char *, ak, const char *, sk, const char *, bucket, const char *, objectKey, unsigned int*, httpStatus, BUFFER_HANDLE, httpResponse)
 
 #ifdef __cplusplus
