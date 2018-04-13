4fdedab605ac93d088860d25698302c17c160906
Bo Zhao <zhaobo03@baidu.com>
2018-04-13 18:28:26 +0800

    fix dm client bug that subscribe many times.

--- iothub_client/src/iotdm_client.c
+++ iothub_client/src/iotdm_client.c
@@ -90,6 +90,7 @@ typedef struct SHADOW_CALLBACK_CONTEXT_TAG
 typedef struct IOTDM_CLIENT_TAG
 {
     bool subscribed;
+    time_t subscribeSentTimestamp;
     char* endpoint;
     char* name;
     IOTHUB_MQTT_CLIENT_HANDLE mqttClient;
@@ -565,6 +566,7 @@ static void ResetIotDmClient(IOTDM_CLIENT_HANDLE handle)
     if (NULL != handle)
     {
         handle->subscribed = false;
+        handle->subscribeSentTimestamp = 0;
 
         handle->endpoint = NULL;
         handle->name = NULL;
@@ -689,6 +691,22 @@ int UpdateShadowWithBinary(const IOTDM_CLIENT_HANDLE handle, const char* device,
     return result;
 }
 
+int OnSubAckCallback(QOS_VALUE* qosReturn, size_t qosCount, void *context)
+{
+    for (int i = 0; i < qosCount; ++i)
+    {
+        if (qosReturn[i] == DELIVER_FAILURE)
+        {
+            LogError("Failed to subscribe");
+            return 0;
+        }
+    }
+    IOTDM_CLIENT_HANDLE handle = context;
+    handle->subscribed = true;
+    LogInfo("Subscribed topics");
+    return 0;
+}
+
 IOTDM_CLIENT_HANDLE iotdm_client_init(char* broker, char* name)
 {
     if (NULL == broker || NULL == name)
@@ -783,30 +801,35 @@ int iotdm_client_dowork(const IOTDM_CLIENT_HANDLE handle)
         return -1;
     }
 
-    handle->subscribed = !(handle->mqttClient->isConnectionLost) && handle->subscribed;
+    if (handle->mqttClient->isConnectionLost)
+    {
+        handle->subscribed = false;
+        handle->subscribeSentTimestamp = 0;
+    }
     if (handle->mqttClient->mqttClientStatus == MQTT_CLIENT_STATUS_CONNECTED && !(handle->subscribed))
     {
-        char* topics[SUB_TOPIC_SIZE];
-        int amount = GetSubscription(handle, topics, SUB_TOPIC_SIZE);
-        if (amount < 0)
-        {
-            LogError("Failure: failed to get the subscribing topics.");
-            return __FAILURE__;
-        }
-        else if (amount > 0)
-        {
-            SUBSCRIBE_PAYLOAD subscribe[SUB_TOPIC_SIZE];
-            for (size_t index = 0; index < (size_t)amount; ++index)
-            {
-                subscribe[index].subscribeTopic = topics[index];
-                subscribe[index].qosReturn = DELIVER_AT_LEAST_ONCE;
-            }
-            int result = subscribe_mqtt_topics(handle->mqttClient, subscribe, amount, NULL, NULL);
-            ReleaseSubscription(topics, SUB_TOPIC_SIZE);
-            if (0 != result)
-            {
-                LogError("Failure: failed to subscribe the topics.");
+        time_t current = time(NULL);
+        double elipsed = difftime(current, handle->subscribeSentTimestamp);
+        if (elipsed > 10) {
+
+            char *topics[SUB_TOPIC_SIZE];
+            int amount = GetSubscription(handle, topics, SUB_TOPIC_SIZE);
+            if (amount < 0) {
+                LogError("Failure: failed to get the subscribing topics.");
                 return __FAILURE__;
+            } else if (amount > 0) {
+                SUBSCRIBE_PAYLOAD subscribe[SUB_TOPIC_SIZE];
+                for (size_t index = 0; index < (size_t) amount; ++index) {
+                    subscribe[index].subscribeTopic = topics[index];
+                    subscribe[index].qosReturn = DELIVER_AT_LEAST_ONCE;
+                }
+                int result = subscribe_mqtt_topics(handle->mqttClient, subscribe, amount, OnSubAckCallback, handle);
+                handle->subscribeSentTimestamp = time(NULL);
+                ReleaseSubscription(topics, SUB_TOPIC_SIZE);
+                if (0 != result) {
+                    LogError("Failure: failed to subscribe the topics.");
+                    return __FAILURE__;
+                }
             }
         }
     }
